25f618ec2e5e109cd221032f3b746c113e8b42d1
AdrSorLop <adsolof2005@gmail.com>
2026-06-19 13:53:23 +0200

    Knowledge section completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
-export default function Card({ img, title, color }) {
+export default function Card({ title, type }) {
 
-    const imgSrc = require(`../images/logos/${img}.png`);
+    const imgSrc = require(`../images/logos/${title}.png`);
+    var color;
 
+    switch(type) {
+        case 'lang': color = '#551768'; break;
+        case 'web': color = '#ccc900'; break;
+        case 'version': color = '#0f500f'; break;
+        default: color = '#000000'; break;
+    }
 
     return(
-        <div className="card" style={{background: `linear-gradient(to bottom, #000000, ${color})` }}>
+        <div className="card" style={{background: `linear-gradient(150deg, #161616, ${color})` }}>
+            <img src={imgSrc} alt="Card image" style={{ paddingTop: '20px' }} />
             <h3>{title}</h3>
-            <img src={imgSrc} alt="Card image"/>
         </div>
     )
 }
--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,10 +1,68 @@
 import React from 'react';
+import Card from '../Card';
 
 export default function Skills() {
   return (
     <section id="skills">
       <h2>Mis conocimientos</h2>
       <p>Lista de tecnologías y habilidades.</p>
+      <h3>Lenguajes de programación</h3>
+      <hr/>
+      <table>
+        <tr>
+          <td>
+            <Card title="Java" type="lang" />
+          </td>
+          <td>
+            <Card title="JavaScript" type="lang" />
+          </td>
+          <td>
+            <Card title="C++" type="lang" />
+          </td>
+          <td>
+            <Card title="Kotlin" type="lang" />
+          </td>
+          <td>
+            <Card title="MySQL" type="lang" />
+          </td>
+        </tr>
+      </table>
+      <h3>Desarrollo web</h3>
+      <hr/>
+      <table>
+        <tr>
+          <td>
+            <Card title="HTML" type="web" />
+          </td>
+          <td>
+            <Card title="CSS" type="web" />
+          </td>
+          <td>
+            <Card title="React" type="web" />
+          </td>
+          <td>
+            <Card title="Bootstrap" type="web" />
+          </td>
+          <td>
+            <Card title="NodeJS" type="web" />
+          </td>
+        </tr>
+      </table>
+      <h3>Control de versiones</h3>
+      <hr/>
+      <table>
+        <tr>
+          <td>
+            <Card title="Git" type="version" />
+          </td>
+          <td>
+            <Card title="GitHub" type="version" />
+          </td>
+          <td>
+            <Card title="Diversion" type="version" />
+          </td>
+        </tr>
+      </table>
     </section>
   );
 }
